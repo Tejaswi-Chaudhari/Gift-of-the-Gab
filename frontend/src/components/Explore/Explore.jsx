30967370f57417643592d4def59b784b5a0a551e
Tejaswi-Chaudhari <tejschaudhari522002@gmail.com>
2021-09-02 22:20:08 +0530

    logo added

--- frontend/src/components/Explore/Explore.jsx
+++ frontend/src/components/Explore/Explore.jsx
@@ -8,72 +8,56 @@ const Explore = () => {
                 <br />
                 <h3 className="display-6" align="center"><strong>- Explore -</strong></h3>
                 <br />
-                <iframe
-                    width="560"
-                    height="315"
+                <iframe className="yt-frame"
                     src="https://www.youtube.com/embed/CzII3W2MLYY"
                     title="YouTube video player"
                     frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen>
                 </iframe>
-                <iframe
-                    width="560"
-                    height="315"
+                <iframe className="yt-frame"
                     src="https://www.youtube.com/embed/sB34sRehUvU"
                     title="YouTube video player"
                     frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen>
                 </iframe>
-                <iframe
-                    width="560"
-                    height="315"
+                <iframe className="yt-frame"
                     src="https://www.youtube.com/embed/fxbCHn6gE3U"
                     title="YouTube video player"
                     frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen>
                 </iframe>
-                <iframe
-                    width="560"
-                    height="315"
+                <iframe className="yt-frame"
                     src="https://www.youtube.com/embed/a2MR5XbJtXU"
                     title="YouTube video player"
                     frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen>
                 </iframe>
-                <iframe
-                    width="560"
-                    height="315"
+                <iframe className="yt-frame"
                     src="https://www.youtube.com/embed/bGBamfWasNQ"
                     title="YouTube video player"
                     frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen>
                 </iframe>
-                <iframe
-                    width="560"
-                    height="315"
+                <iframe className="yt-frame"
                     src="https://www.youtube.com/embed/RKK7wGAYP6k"
                     title="YouTube video player"
                     frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen>
                 </iframe>
-                <iframe
-                    width="560"
-                    height="315"
+                <iframe className="yt-frame"
                     src="https://www.youtube.com/embed/JoUZ929qoLk"
                     title="YouTube video player"
                     frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen>
                 </iframe>
-                <iframe
-                    width="560"
-                    height="315"
+                <iframe className="yt-frame"
                     src="https://www.youtube.com/embed/N2QZM7azGoA"
                     title="YouTube video player"
                     frameborder="0"
